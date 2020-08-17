76b8716dfc677ffc7bdf01413e24401669630676
don-riel <62613405+don-riel@users.noreply.github.com>
2020-08-17 19:47:11 +0200

    implemented grid layout on hero images

--- src/components/hero/hero.component.js
+++ src/components/hero/hero.component.js
@@ -10,16 +10,18 @@ const Hero = () => {
         <div className={styles.hero_container}>
             <Img 
                 fluid={data.trolltunga2.childImageSharp.fluid}
-                className={styles.hero_img} 
+                className={`${styles.hero_img} ${styles.left_img}`} 
             />
             <Img 
                 fluid={data.trolltunga.childImageSharp.fluid}
-                className={styles.hero_img} 
+                className={`${styles.hero_img} ${styles.middle_img}`} 
             />
             <Img 
                 fluid={data.rampestreken.childImageSharp.fluid}
-                className={styles.hero_img} 
-            />    
+                className={`${styles.hero_img} ${styles.right_img}`} 
+            />  
+            <div className={styles.white}></div>
+            <div className={styles.shadow}></div>   
         </div>
         
 
