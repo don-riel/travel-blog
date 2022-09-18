8d05e8efdd8c0ceb32720802dad004bd3a27bd57
Donni <donni.garvida@gmail.com>
2022-09-18 09:14:20 +0200

    change nav and footer styling

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -9,8 +9,41 @@ const Footer = () => {
 
   return (
     <div className={styles.footer}>
+      <div className={styles.social_media}>
+        <p className={styles.section_title}>Follow me</p>
+
+        <div className={styles.social_icons}>
+          <a href="https://www.youtube.com/channel/UCYxJshfIpMfvvc7psztDWiw/featured" className={styles.social_link}>
+            <Img
+              fluid={images.youtube.childImageSharp.fluid}
+              alt="email icon"
+              className={styles.social_media_img}
+            />
+            <span>YouTube</span>
+          </a>
+          <a href="https://www.facebook.com/karolanne.ramoran" className={styles.social_link}>
+            <Img
+              fluid={images.facebook.childImageSharp.fluid}
+              alt="email icon"
+              className={styles.social_media_img}
+            />
+            <span>Facebook</span>
+          </a>
+
+          <a href="https://www.instagram.com/imannakarolina/" className={styles.social_link}>
+            {" "}
+            <Img
+              fluid={images.instagram.childImageSharp.fluid}
+              alt="email icon"
+              className={styles.social_media_img}
+            />
+            <span>Instagram</span>
+          </a>
+        </div>
+      </div>
+    
       <div className={styles.contact_info}>
-        <p>Contact</p>
+        <p className={styles.section_title}>Contact</p>
 
         <div className={styles.email_info}>
           <Img
@@ -18,39 +51,10 @@ const Footer = () => {
             alt="email icon"
             className={styles.social_media_img}
           />
-          <p className={styles.email_text}>karolannebramoran@gmail.com</p>
+          <span className={styles.email_text}>karolannebramoran@gmail.com</span>
         </div>
       </div>
 
-      <div className={styles.social_media}>
-        <p>Follow me</p>
-
-        <div className={styles.social_icons}>
-          <a href="https://www.youtube.com/channel/UCYxJshfIpMfvvc7psztDWiw/featured">
-            <Img
-              fluid={images.youtube.childImageSharp.fluid}
-              alt="email icon"
-              className={styles.social_media_img}
-            />
-          </a>
-          <a href="https://www.facebook.com/karolanne.ramoran">
-            <Img
-              fluid={images.facebook.childImageSharp.fluid}
-              alt="email icon"
-              className={styles.social_media_img}
-            />
-          </a>
-
-          <a href="https://www.instagram.com/imannakarolina/">
-            {" "}
-            <Img
-              fluid={images.instagram.childImageSharp.fluid}
-              alt="email icon"
-              className={styles.social_media_img}
-            />
-          </a>
-        </div>
-      </div>
 
       <div className={styles.copyright}>
         <p>Copyright © {new Date().getFullYear()} Karol Anne</p>
